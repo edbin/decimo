e5a547c870f90c0f2958a3eedc933ba3b6268f8b
edbin <73134283+edbin@users.noreply.github.com>
2024-10-15 12:11:43 -0600

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+$(".menu > ul > li").click(function (e) {
+    $(this).siblings().removeClass("active");
+    $(this).toggleClass("active");
+    $(this).find("ul").slideToggle();
+    $(this).siblings().find("ul").slideUp();
+    $(this).siblings().find("li").removeClass("active");
+});
