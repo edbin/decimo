1956c06451dab78fa42fba06e2f8c3329612f28f
edbin <73134283+edbin@users.noreply.github.com>
2024-10-22 17:02:36 -0600

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-$(".menu > ul > li").click(function (e) {
-    $(this).siblings().removeClass("active");
-    $(this).toggleClass("active");
-    $(this).find("ul").slideToggle();
-    $(this).siblings().find("ul").slideUp();
-    $(this).siblings().find("li").removeClass("active");
-});
